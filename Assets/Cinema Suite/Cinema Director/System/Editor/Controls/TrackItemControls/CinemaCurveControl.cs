163b4c5d1af1c74be977d5da97f286b2b3d5bad8
StrangeTang <15927678827@163.com>
2017-03-02 20:56:54 +0800

    init

---
+++ Assets/Cinema Suite/Cinema Director/System/Editor/Controls/TrackItemControls/CinemaCurveControl.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using CinemaDirector;
+
+[CutsceneItemControlAttribute(typeof(CinemaClipCurve))]
+public class CinemaCurveControl : CinemaCurveClipItemControl
+{
+    protected bool hasUndoRedoBeenPerformed = false;
+
+    public CinemaCurveControl()
+    {
+        base.AlterDuration += CinemaCurveControl_AlterDuration;
+        base.AlterFiretime += CinemaCurveControl_AlterFiretime;
+        base.TranslateCurveClipItem += CinemaCurveControl_TranslateCurveClipItem;
+        base.SnapScrubber += CinemaCurveControl_SnapScrubber;
+    }
+
+    void CinemaCurveControl_SnapScrubber(object sender, CurveClipScrubberEventArgs e)
+    {
+        CinemaClipCurve curveClip = e.curveClipItem as CinemaClipCurve;
+        if (curveClip == null) return;
+
+        curveClip.Cutscene.SetRunningTime(e.time);
+    }
+
+    void CinemaCurveControl_TranslateCurveClipItem(object sender, CurveClipItemEventArgs e)
+    {
+        CinemaClipCurve curveClip = e.curveClipItem as CinemaClipCurve;
+        if (curveClip == null) return;
+
+        Undo.RecordObject(e.curveClipItem, string.Format("Changed {0}", curveClip.name));
+        curveClip.TranslateCurves(e.firetime - curveClip.Firetime);
+        EditorUtility.SetDirty(e.curveClipItem);
+    }
+
+    void CinemaCurveControl_AlterFiretime(object sender, CurveClipItemEventArgs e)
+    {
+        CinemaClipCurve curveClip = e.curveClipItem as CinemaClipCurve;
+        if (curveClip == null) return;
+
+        Undo.RecordObject(e.curveClipItem, string.Format("Changed {0}", curveClip.name));
+        curveClip.AlterFiretime(e.firetime, e.duration);
+
+        if (e.duration <= 0)
+        {
+            GUIUtility.hotControl = 0;
+            deleteItem(e.curveClipItem);
+        }
+        EditorUtility.SetDirty(e.curveClipItem);
+    }
+
+    void CinemaCurveControl_AlterDuration(object sender, CurveClipItemEventArgs e)
+    {
+        CinemaClipCurve curveClip = e.curveClipItem as CinemaClipCurve;
+        if (curveClip == null) return;
+
+        Undo.RecordObject(e.curveClipItem, string.Format("Changed {0}", curveClip.name));
+        curveClip.AlterDuration(e.duration);
+
+        if (e.duration <= 0)
+        {
+            GUIUtility.hotControl = 0;
+            deleteItem(e.curveClipItem);
+        }
+        EditorUtility.SetDirty(e.curveClipItem);
+    }
+
+    public override void UpdateCurveWrappers(CinemaClipCurveWrapper clipWrapper)
+    {
+        CinemaClipCurve clipCurve = clipWrapper.Behaviour as CinemaClipCurve;
+        if (clipCurve == null) return;
+        
+        for (int i = 0; i < clipCurve.CurveData.Count; i++)
+        {
+            MemberClipCurveData member = clipCurve.CurveData[i];
+
+            CinemaMemberCurveWrapper memberWrapper = null;
+            if (!clipWrapper.TryGetValue(member.Type, member.PropertyName, out memberWrapper))
+            {
+                memberWrapper = new CinemaMemberCurveWrapper();
+                memberWrapper.Type = member.Type;
+                memberWrapper.PropertyName = member.PropertyName;
+                memberWrapper.Texture = EditorGUIUtility.ObjectContent(null, UnityPropertyTypeInfo.GetUnityType(member.Type)).image;
+                ArrayUtility.Add<CinemaMemberCurveWrapper>(ref clipWrapper.MemberCurves, memberWrapper);
+
+                int showingCurves = UnityPropertyTypeInfo.GetCurveCount(member.PropertyType);
+                memberWrapper.AnimationCurves = new CinemaAnimationCurveWrapper[showingCurves];
+
+                for (int j = 0; j < showingCurves; j++)
+                {
+                    memberWrapper.AnimationCurves[j] = new CinemaAnimationCurveWrapper();
+
+                    memberWrapper.AnimationCurves[j].Id = j;
+                    memberWrapper.AnimationCurves[j].Curve = member.GetCurve(j);
+                    memberWrapper.AnimationCurves[j].Label = UnityPropertyTypeInfo.GetCurveName(member.PropertyType, j);
+                    memberWrapper.AnimationCurves[j].Color = UnityPropertyTypeInfo.GetCurveColor(member.Type, member.PropertyName, memberWrapper.AnimationCurves[j].Label, j);
+                }
+            }
+        }
+
+        // Remove missing track items
+        List<CinemaMemberCurveWrapper> itemRemovals = new List<CinemaMemberCurveWrapper>();
+        foreach (CinemaMemberCurveWrapper cw in clipWrapper.MemberCurves)
+        {
+            bool found = false;
+            foreach (MemberClipCurveData member in clipCurve.CurveData)
+            {
+                if (member.Type == cw.Type && member.PropertyName == cw.PropertyName)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                itemRemovals.Add(cw);
+            }
+        }
+        foreach (CinemaMemberCurveWrapper item in itemRemovals)
+        {
+            ArrayUtility.Remove<CinemaMemberCurveWrapper>(ref clipWrapper.MemberCurves, item);
+        }
+    }
+}
---
+++ Assets/Cinema Suite/Cinema Mocap/Runtime/RealtimeHumanoidPosing.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RealtimeHumanoidPosing : MonoBehaviour
+{
+    private GameObject CHARACTER;
+    private GameObject HIP;
+    private GameObject SPINE;
+    private GameObject SHOULDER_CENTER;
+    private GameObject HEAD;
+    private GameObject SHOULDER_LEFT;
+    private GameObject ELBOW_LEFT;
+    private GameObject WRIST_LEFT;
+    private GameObject HAND_LEFT;
+    private GameObject SHOULDER_RIGHT;
+    private GameObject ELBOW_RIGHT;
+    private GameObject WRIST_RIGHT;
+    private GameObject HAND_RIGHT;
+    private GameObject HIP_LEFT;
+    private GameObject KNEE_LEFT;
+    private GameObject ANKLE_LEFT;
+    private GameObject FOOT_LEFT;
+    private GameObject HIP_RIGHT;
+    private GameObject KNEE_RIGHT;
+    private GameObject ANKLE_RIGHT;
+    private GameObject FOOT_RIGHT;
+
+    private GameObject[] joints;
+
+    private Vector3 startingPosition;
+
+    public void Awake()
+    {
+        CHARACTER = this.gameObject;
+
+        if (CHARACTER != null)
+        {
+            HIP = CHARACTER.transform.FindChild("HIP").gameObject;
+
+            HIP_LEFT = HIP.transform.FindChild("HIP_LEFT").gameObject;
+            KNEE_LEFT = HIP_LEFT.transform.FindChild("KNEE_LEFT").gameObject;
+            ANKLE_LEFT = KNEE_LEFT.transform.FindChild("ANKLE_LEFT").gameObject;
+            FOOT_LEFT = ANKLE_LEFT.transform.FindChild("FOOT_LEFT").gameObject;
+
+            HIP_RIGHT = HIP.transform.FindChild("HIP_RIGHT").gameObject;
+            KNEE_RIGHT = HIP_RIGHT.transform.FindChild("KNEE_RIGHT").gameObject;
+            ANKLE_RIGHT = KNEE_RIGHT.transform.FindChild("ANKLE_RIGHT").gameObject;
+            FOOT_RIGHT = ANKLE_RIGHT.transform.FindChild("FOOT_RIGHT").gameObject;
+
+            SPINE = HIP.transform.FindChild("SPINE").gameObject;
+            SHOULDER_CENTER = SPINE.transform.FindChild("SHOULDER_CENTER").gameObject;
+            HEAD = SHOULDER_CENTER.transform.FindChild("HEAD").gameObject;
+
+            SHOULDER_LEFT = SHOULDER_CENTER.transform.FindChild("SHOULDER_LEFT").gameObject;
+            ELBOW_LEFT = SHOULDER_LEFT.transform.FindChild("ELBOW_LEFT").gameObject;
+            WRIST_LEFT = ELBOW_LEFT.transform.FindChild("WRIST_LEFT").gameObject;
+            HAND_LEFT = WRIST_LEFT.transform.FindChild("HAND_LEFT").gameObject;
+
+            SHOULDER_RIGHT = SHOULDER_CENTER.transform.FindChild("SHOULDER_RIGHT").gameObject;
+            ELBOW_RIGHT = SHOULDER_RIGHT.transform.FindChild("ELBOW_RIGHT").gameObject;
+            WRIST_RIGHT = ELBOW_RIGHT.transform.FindChild("WRIST_RIGHT").gameObject;
+            HAND_RIGHT = WRIST_RIGHT.transform.FindChild("HAND_RIGHT").gameObject;
+        }
+
+        joints = new GameObject[25] {
+			null, HIP, SPINE, SHOULDER_CENTER,
+			null, SHOULDER_LEFT, ELBOW_LEFT, WRIST_LEFT,
+			null, SHOULDER_RIGHT, ELBOW_RIGHT, WRIST_RIGHT,
+			HIP, HIP_LEFT, KNEE_LEFT, ANKLE_LEFT,
+			null, HIP_RIGHT, KNEE_RIGHT, ANKLE_RIGHT,
+			HEAD, HAND_LEFT, HAND_RIGHT, FOOT_LEFT, FOOT_RIGHT};
+    }
+
+    public void SetWorldPosition(Vector3 position)
+    {
+        if (startingPosition == Vector3.zero)
+        {
+            startingPosition = position - CHARACTER.transform.position;
+        }
+        CHARACTER.transform.position = position - startingPosition;
+    }
+
+    public void SetRotations(Quaternion[] rotations)
+    {
+        for (int i = 0; i < rotations.Length; i++)
+        {
+            if (joints[i] != null)
+            {
+                joints[i].transform.localRotation = rotations[i];
+            }
+        }
+    }
+}
+
